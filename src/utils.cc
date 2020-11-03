dfecab960babbac909430bc68486a835d0c4fb36
qinhj <qinhongjie@longcheer.com>
2020-11-03 11:02:19 +0800

    [src] code review: utils.h/cc

--- src/utils.cc
+++ src/utils.cc
@@ -6,48 +6,61 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// 2020/11/03   reviewed by qinhj@lsec.cc.ac.cn
+
 #include "utils.h"
 
-#include <iomanip>
-#include <ios>
+#include <iomanip>  // for: std::setw
+#include <ios>      // for: ios::end
 
 namespace fasttext {
 
-namespace utils {
+    namespace utils {
 
-int64_t size(std::ifstream& ifs) {
-  ifs.seekg(std::streamoff(0), std::ios::end);
-  return ifs.tellg();
-}
+        // @brief:  get input file stream size
+        int64_t size(std::ifstream& ifs) {
+            // =========================================
+            // Note:
+            //  streampos: absolute position of stream;
+            //  streamoff: relative offset of stream;
+            // =========================================
+            ifs.seekg(std::streamoff(0), std::ios::end);
+            // return the distance between start and end posistions of stream
+            return ifs.tellg();
+        }
 
-void seek(std::ifstream& ifs, int64_t pos) {
-  ifs.clear();
-  ifs.seekg(std::streampos(pos));
-}
+        // @brief:  set stream to input position
+        void seek(std::ifstream& ifs, int64_t pos) {
+            ifs.clear();
+            ifs.seekg(std::streampos(pos));
+        }
 
-double getDuration(
-    const std::chrono::steady_clock::time_point& start,
-    const std::chrono::steady_clock::time_point& end) {
-  return std::chrono::duration_cast<std::chrono::duration<double>>(end - start)
-      .count();
-}
+        // @brief:  get duration(seconds) between input time points
+        double getDuration(
+            const std::chrono::steady_clock::time_point& start,
+            const std::chrono::steady_clock::time_point& end) {
+            return std::chrono::duration_cast<std::chrono::duration<double>>(end - start)
+                .count();
+        }
 
-ClockPrint::ClockPrint(int32_t duration) : duration_(duration) {}
+        ClockPrint::ClockPrint(int32_t duration) : duration_(duration) {
+        }
 
-std::ostream& operator<<(std::ostream& out, const ClockPrint& me) {
-  int32_t etah = me.duration_ / 3600;
-  int32_t etam = (me.duration_ % 3600) / 60;
-  int32_t etas = (me.duration_ % 3600) % 60;
+        // operator overload: "<<"
+        std::ostream& operator<<(std::ostream& out, const ClockPrint& me) {
+            int32_t etah = me.duration_ / 3600;
+            int32_t etam = (me.duration_ % 3600) / 60;
+            int32_t etas = (me.duration_ % 3600) % 60;
 
-  out << std::setw(3) << etah << "h" << std::setw(2) << etam << "m";
-  out << std::setw(2) << etas << "s";
-  return out;
-}
+            out << std::setw(3) << etah << "h" << std::setw(2) << etam << "m";
+            out << std::setw(2) << etas << "s";
+            return out;
+        }
 
-bool compareFirstLess(const std::pair<double, double>& l, const double& r) {
-  return l.first < r;
-}
+        bool compareFirstLess(const std::pair<double, double>& l, const double& r) {
+            return l.first < r;
+        }
 
-} // namespace utils
+    } // namespace utils
 
 } // namespace fasttext
--- src/utils.h
+++ src/utils.h
@@ -6,67 +6,98 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+// ===============================================================
+// History:
+// 2020/11/03   reviewed by qinhj@lsec.cc.ac.cn
+// ---------------------------------------------------------------
+// Note:
+// 1) To use lambda function and std::chrono, we need: -std=c++11.
+// ===============================================================
+
 #pragma once
 
+/* user includes */
 #include "real.h"
-
-#include <algorithm>
-#include <chrono>
-#include <fstream>
-#include <ostream>
-#include <vector>
+/* c++ includes */
+#include <algorithm>// for: std::find_if
+#include <chrono>   // for: std::chrono
+#include <fstream>  // for: std::ifstream
+#include <ostream>  // for: std::ostream
+#include <vector>   // for: std::vector
 
 #if defined(__clang__) || defined(__GNUC__)
 #define FASTTEXT_DEPRECATED(msg) __attribute__((__deprecated__(msg)))
 #elif defined(_MSC_VER)
 #define FASTTEXT_DEPRECATED(msg) __declspec(deprecated(msg))
-#else
+#else   /* unknown */
 #define FASTTEXT_DEPRECATED(msg)
-#endif
+#endif  /* compiler */
 
 namespace fasttext {
 
-using Predictions = std::vector<std::pair<real, int32_t>>;
+    using Predictions = std::vector<std::pair<real, int32_t>>;
 
-namespace utils {
+    namespace utils {
 
-int64_t size(std::ifstream&);
+        // @brief:  get input file stream size
+        int64_t size(std::ifstream&);
 
-void seek(std::ifstream&, int64_t);
+        // @brief:  set stream to input position
+        void seek(std::ifstream&, int64_t);
 
-template <typename T>
-bool contains(const std::vector<T>& container, const T& value) {
-  return std::find(container.begin(), container.end(), value) !=
-      container.end();
-}
+        // @brief:  check if input value of type T is contained or not
+        template <typename T>
+        bool contains(const std::vector<T>& container, const T& value) {
+            // ========================================================
+            // Note: bool operator==(const T &rhs) const; is required
+            // ========================================================
+            return std::find(container.begin(), container.end(), value) !=
+                container.end();
+        }
+        // @brief:  check if input value of type T2 is contained or not
+        template <typename T1, typename T2>
+        bool containsSecond(
+            const std::vector<std::pair<T1, T2>>& container,
+            const T2& value) {
+            return std::find_if(
+                container.begin(),
+                container.end(),
+                // ====================================================
+                // Note: lambda function with input reference of value,
+                // and parameter "const std::pair<T1, T2>& item".
+                // ====================================================
+                [&value](const std::pair<T1, T2>& item)
+            {
+                return item.second == value;
+            }) != container.end();
+        }
 
-template <typename T1, typename T2>
-bool containsSecond(
-    const std::vector<std::pair<T1, T2>>& container,
-    const T2& value) {
-  return std::find_if(
-             container.begin(),
-             container.end(),
-             [&value](const std::pair<T1, T2>& item) {
-               return item.second == value;
-             }) != container.end();
-}
+        // @brief:  get duration(seconds) between input time points
+        // @example:
+        //  using namespace std::chrono;
+        //  steady_clock::time_point t1 = steady_clock::now();
+        //  for (int i = 0; i<1000; ++i) std::cout << "*";
+        //  steady_clock::time_point t2 = steady_clock::now();
+        //  std::cout << "It took me " << getDuration(t1, t2) << " seconds.";
+        double getDuration(
+            // ========================================================
+            // Note: For more details, one can visit the following url
+            // http://www.cplusplus.com/reference/chrono/steady_clock
+            // ========================================================
+            const std::chrono::steady_clock::time_point& start,
+            const std::chrono::steady_clock::time_point& end);
 
-double getDuration(
-    const std::chrono::steady_clock::time_point& start,
-    const std::chrono::steady_clock::time_point& end);
+        class ClockPrint {
+        public:
+            explicit ClockPrint(int32_t duration);
+            friend std::ostream& operator<<(std::ostream& out, const ClockPrint& me);
 
-class ClockPrint {
- public:
-  explicit ClockPrint(int32_t duration);
-  friend std::ostream& operator<<(std::ostream& out, const ClockPrint& me);
+        private:
+            int32_t duration_;
+        };
 
- private:
-  int32_t duration_;
-};
+        bool compareFirstLess(const std::pair<double, double>& l, const double& r);
 
-bool compareFirstLess(const std::pair<double, double>& l, const double& r);
-
-} // namespace utils
+    } // namespace utils
 
 } // namespace fasttext
